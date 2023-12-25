825b054af71df37a2db4adacb32e8fde5336a7dd
yuz <yuzyuzx@gmail.com>
2023-12-25 20:15:27 +0900

    ひとまずlsp導入

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -55,16 +55,160 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  { "neovim/nvim-lspconfig" },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  -- { "L3MON4D3/LuaSnip" },
+  -- { "saadparwaiz1/cmp_luasnip" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { 'windwp/nvim-autopairs', event = "InsertEnter", opts = {} },
+  { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
   { "nvim-tree/nvim-web-devicons", lazy = true },
-  { 'numToStr/Comment.nvim', opts = {}, lazy = false, },
+  { "numToStr/Comment.nvim", opts = {}, lazy = false, },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { 'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons' },
-  'vim-jp/vimdoc-ja',
-  'nvim-lualine/lualine.nvim',
-  -- 'windwp/nvim-ts-autotag',
+  { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+  "vim-jp/vimdoc-ja",
+  "nvim-lualine/lualine.nvim",
+  -- "windwp/nvim-ts-autotag",
 })
+
+local on_attach = function(client, bufnr)
+  -- LSPサーバーのフォーマット機能を無効にするには下の行をコメントアウト
+  -- 例えばtypescript-language-serverにはコードのフォーマット機能が付いているが代わりにprettierでフォーマットしたいときなど
+  -- client.resolved_capabilities.document_formatting = false
+
+  local set = vim.keymap.set
+  set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+  set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  set("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
+  set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
+  set("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
+  set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+  set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+  set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+  set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+  set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>")
+  set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+end
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function (server_name)
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach
+    }
+  end,
+}
+
+capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local cmp = require"cmp"
+cmp.setup({
+  snippet = {
+    -- expand = function(args)
+    --   require("luasnip").lsp_expand(args.body)
+    -- end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    -- { name = "luasnip" },
+  }, {
+    -- { name = "buffer" },
+  })
+})
+
+-- 1. LSP Sever management
+-- require('mason').setup()
+-- require("mason-lspconfig").setup()
+-- require('mason-lspconfig').setup_handlers({ function(server)
+--   local opt = {
+--     -- -- Function executed when the LSP server startup
+--     -- on_attach = function(client, bufnr)
+--     --   local opts = { noremap=true, silent=true }
+--     --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+--     --   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
+--     -- end,
+    -- capabilities = require('cmp_nvim_lsp').update_capabilities(
+    --   vim.lsp.protocol.make_client_capabilities()
+    -- )
+--   }
+--   require('lspconfig')[server].setup(opt)
+-- end })
+
+
+-- 2. build-in LSP function
+-- keyboard shortcut
+-- vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+-- vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+-- vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+-- vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+-- vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+-- vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+-- vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+-- vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+-- vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+-- vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+-- vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+-- vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+-- -- LSP handlers
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+--   vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
+-- )
+-- Reference highlight
+-- vim.cmd [[
+-- set updatetime=500
+-- highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+-- highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+-- highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+-- augroup lsp_document_highlight
+--   autocmd!
+--   autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+--   autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+-- augroup END
+-- ]]
+
+-- 3. completion (hrsh7th/nvim-cmp)
+-- local cmp = require("cmp")
+-- cmp.setup({
+--   snippet = {
+--     -- expand = function(args)
+--     --   vim.fn["vsnip#anonymous"](args.body)
+--     -- end,
+--   },
+--   sources = {
+--     { name = "nvim_lsp" },
+--     -- { name = "buffer" },
+--     -- { name = "path" },
+--   },
+--   mapping = cmp.mapping.preset.insert({
+--     ["<C-p>"] = cmp.mapping.select_prev_item(),
+--     ["<C-n>"] = cmp.mapping.select_next_item(),
+--     ['<C-l>'] = cmp.mapping.complete(),
+--     ['<C-e>'] = cmp.mapping.abort(),
+--     ["<CR>"] = cmp.mapping.confirm { select = true },
+--   }),
+--   experimental = {
+--     -- ghost_text = true,
+--   },
+-- })
 
 -- colorscheme
 --[[
